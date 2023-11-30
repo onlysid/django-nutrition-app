5dd1ed6d869dd6b59c54fab52487c2e53402c746
Sidney <sid@onlysid.com>
2023-11-30 12:26:52 +0000

    finished basic app with no styling.

--- calories/urls.py
+++ calories/urls.py
@@ -1,8 +1,9 @@
 from django.contrib import admin
 from django.urls import path
 from .views import index
+from tracker import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', index, name='index')
+    path('', views.index, name='index')
 ]
--- tracker/views.py
+++ tracker/views.py
@@ -1,3 +1,40 @@
 from django.shortcuts import render
+from .models import Consume, Food
 
 # Create your views here.
+def index(request):
+    
+    if request.method == 'POST':
+        food_consumed = request.POST['foodConsumed']
+        consume = Food.objects.get(pk=food_consumed)
+        user = request.user
+        
+        consume = Consume(food=consume, user=user)
+        consume.save()
+        
+    elif request.method == 'GET' and 'delete' in request.GET:
+        id = request.GET['delete']
+        
+        try:
+            consumed_food = Consume.objects.get(pk=id)
+        except Consume.DoesNotExist:
+            consumed_food = None
+        if consumed_food and consumed_food.user == request.user:
+            consumed_food.delete()
+        
+    consumed_food = Consume.objects.filter(user=request.user)
+    foods = Food.objects.all()    
+    
+    context = {
+        'foods': foods,
+        'consumed_food': consumed_food,
+    }
+    
+    return render(request, 'tracker/index.html', context)
+
+
+def delete_consume(request, id):
+    consumed_food = Consume.objects.get(pk=id)
+    consumed_food.delete()
+    
+    return render(request, 'tracker/index.html')
